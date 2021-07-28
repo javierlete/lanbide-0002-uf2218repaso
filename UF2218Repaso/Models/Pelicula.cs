d93c1fc6736f8d2d30e97888d05fafb0349ff22c
Javier Lete <javier@eskura21.com>
2021-07-28 09:25:34 +0200

    Modelo de película

---
+++ UF2218Repaso/Models/Pelicula.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UF2218Repaso.Models
+{
+    public class Pelicula
+    {
+        public long Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Pelicula(long id, string titulo, DateTime fecha)
+        {
+            Id = id;
+            Titulo = titulo;
+            Fecha = fecha;
+        }
+
+        public Pelicula() { }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Titulo}, {Fecha}";
+        }
+    }
+}
