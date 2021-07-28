97e4d2426c7132f0030388aee9feb9e8d6fd6be9
Javier Lete <javier@eskura21.com>
2021-07-28 09:21:47 +0200

    Contenido página Maestra con Bootstrap y FontAwesome

--- UF2218Repaso/Base.Master.designer.cs
+++ UF2218Repaso/Base.Master.designer.cs
@@ -33,12 +33,12 @@ namespace UF2218Repaso
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Control ContentPlaceHolder1.
+        /// Control contenido.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder contenido;
     }
 }
--- UF2218Repaso/Default.aspx
+++ UF2218Repaso/Default.aspx
@@ -1,5 +1,6 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Base.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="UF2218Repaso.Default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<asp:Content ID="Content2" ContentPlaceHolderID="contenido" runat="server">
+    <h1 class="display-1">Bienvenidos al ejemplo</h1>
 </asp:Content>
